fe3f03b6cbfa4757e502f8b8a78cbf89cb1daf26
adnanabrar786 <adnan1245abrar@gmail.com>
2022-11-14 23:53:07 -0800

    commit

--- index.js
+++ index.js
@@ -21,14 +21,7 @@ fruits.splice(3, 0, "Lemon", "Kiwi");
 // Result :::
 // *) add value in Array
 
-//   ::::::::::::::::  Topic Name : Math.ceil and  Math.floor ::::::::::::::::
-let myNumber = 22.22;
-let resultCeil = Math.ceil(myNumber);
-let resultFloor = Math.floor(myNumber);
-// console.log(resultCeil);
-// console.log(resultFloor);
-
-//   ::::::::::::::::  Topic Name : Math.ceil and  array.map() ::::::::::::::::
+//   ::::::::::::::::  Topic Name : array.map() ::::::::::::::::
 const array = [1, 6, 9, 16];
 
 // pass a function to map
@@ -93,3 +86,44 @@ const resultthird = inventory.find((fruit) => {
 // *) Return a specific value
 // *) not return array
 
+
+//   ::::::::::::::::  Topic Name : Math.ceil and  Math.floor ::::::::::::::::
+let myNumber = 22.22;
+let resultCeil = Math.ceil(myNumber);
+let resultFloor = Math.floor(myNumber);
+// console.log(resultCeil);
+// console.log(resultFloor);
+
+
+//   ::::::::::::::::  Topic Name : arr.reverse ::::::::::::::::
+// var myarr = [34, 234, 567, 4];
+// var  reverseArr = myarr.reverse();
+// console.log(reverseArr);
+
+// Result :::
+// *) Return reverse array
+// *) apply in array
+// *) not apply in string
+
+
+
+
+continue this Look    ......................................
+
+//   ::::::::::::::::  Topic Name : str.split ::::::::::::::::
+// const str = "adnan is best";
+// const arr = str.split(" ");
+// const a = arr.reverse();
+// console.log(a);
+
+// Result :::
+// *) Return reverse array
+// *) apply in string
+// *) it convert string into array
+//  by given condtion
+// *) not apply in array
+
+
+const str = "this is a test string";
+const reverseStr = str.split(" ").reverse().join("");
+console.log(reverseStr);
