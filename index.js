fb8d1e7bec9bc3893e17bff6c0a0552ce92f62ab
adnanabrar786 <adnan1245abrar@gmail.com>
2022-11-14 16:44:58 -0800

    commit

--- index.js
+++ index.js
@@ -1,129 +1,30 @@
-// setTimeout + Closures Interview Question | Namaste  JavaScript Ep. 11
+
+//   ::::::::::::::::  Topic Name : Array.slice ::::::::::::::::
+const sliceArray = [1, 2, 3, 4, 5, 6, 7];
+const newArray = sliceArray.slice(1, 5);
+// sliceArray.slice(startingIndex , endIndex)
+
+console.log(sliceArray);
+console.log(newArray);
+// Result :::
+// *) Return New Array
+
+//   ::::::::::::::::  Topic Name : Array.splice ::::::::::::::::
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+//  At position 2, remove 0 , add 2 elements:
+fruits.splice(3, 0, "Lemon", "Kiwi");
+//  fruits.splice(index, remove value, [ Elements]);
+console.log(fruits);
+
+// Result :::
+// *) add value in Array
 
 
-// let b = 40 ;
-const a = () => {
+//   ::::::::::::::::  Topic Name : Math.ceil and  Math.floor ::::::::::::::::
+let myNumber = 22.22;
+let resultCeil = Math.ceil(myNumber);
+let resultFloor = Math.floor(myNumber)
+console.log(resultCeil);
+console.log(resultFloor);
 
-    for(i = 0 ; i <= 5; i++){
-        setTimeout(() => {
-            console.log(i);
-        }, i * 3000);
-    }
-
-    console.log("hello world");
-}
-
-a();
-
-
-// Closures in JS  | Namaste JavaScript Episode 10
-// function a(){
-//    let a = 7;
-//    return function b () {
-//    console.log(a);
-//    }
-// }
-
-// let z = a();
-// console.log(z);
-// z();
-
-
-// BLOCK SCOPE & Shadowing in JS | Namaste JavaScript Ep. 9
-
-// let a = 100;
-// {
-//    a = 10;
-// }
-// console.log(a);
-// console.log(e);
-// console.log(f);
-
-// // lexical environment
-// function a() {
-//     var b = 10;
-//     c();
-//     function c(){
-//         console.log(b);
-//     }
-// }
-// a();
-
-// function a() {
-//     console.log(b);
-// }
-
-// var b = 10 ;
-// a();
-
-// var x= 1;
-// a();
-// b();
-
-// console.log(x);
-
-// function a() {
-//      var x = 10;
-//      console.log(x);
-// }
-
-// function b() {
-//      var x = 100;
-//      console.log(x);
-// }
-
-// myfunc();
-
-// console.log(x);
-
-// const x = "23";
-
-// function myfunc(a, b, c, d) {
-//   console.log(arguments);
-// }
-// myfunc("1", "2", "3", "4");
-
-// const arrowfunc = function(a, b, c, d)  {
-//     console.log(arguments,">>>>>>>>>");
-// };
-
-// arrowfunc("1", "2", "3", "4");
-
-// console.log(arrowfunc);
-
-// function myfunctwo() {
-//   console.log(arguments);
-// }
-
-// myfunctwo();
-// console.log("hello world");
-
-// extra practice
-// function subtract(...[a,b]){
-//     return a- b;
-// }
-
-// var x = subtract(10);
-// var y = subtract(10, 5);
-// var z = subtract(10, 5, 5);
-
-// console.log(x);
-// console.log(y);
-// console.log(z);
-
-// console.log(myfunc());
-// console.log(x);
-
-// var x= 1;
-// a();
-// b();
-
-// function a() {
-//      var x = 10;
-//      console.log(x);
-// }
-
-// function b() {
-//      var x = 100;
-//      console.log(x);
-// }
