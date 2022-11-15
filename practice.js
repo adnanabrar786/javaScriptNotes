1b03d3ceca2126299ac23946e5e50a64676037a1
adnanabrar786 <adnan1245abrar@gmail.com>
2022-11-14 16:37:19 -0800

    commit

---
+++ practice.js
@@ -0,0 +1,82 @@
+// https://contactmentor.com/javascript-if-else-exercises-solutions/
+
+// const myfunc = (age) => {
+//   if (age <= 18) {
+//     console.log(`Not Allow This Place You are Younger${age}`);
+//   } else if (age <= 48) {
+//     console.log(`Allow This Place you are eligible :-) ${age}`);
+//   } else if (48 <= age) {
+//     console.log(`not Allow You are older ${age}`);
+//   }
+// };
+
+// myfunc(37);
+
+// let i = 22;
+
+// for (i = 0; i <= 20; i++) {
+//   if (i % 2 === 0) {
+//     console.log(`The number is even ${i}`);
+//   } else {
+//     console.log(`The number is odd ${i}`);
+//   }
+// }
+
+// let i = "222";
+
+// const myfunc = () => {
+//   if (Number(i)) {
+//     console.log("number");
+//   } else {
+//     console.log("not a number");
+//   }
+// };
+
+// myfunc();
+
+// const myfunc = (nunone, numtwo) => {
+//   if (nunone > numtwo) {
+//     console.log("number one");
+//   } else {
+//     console.log("number two");
+//   }
+// };
+
+// myfunc(22, 10);
+
+// find name in input value if name is available is answer me its true otherwise False
+// const obj = [
+//   {
+//     firstName: "ali",
+//   },
+//   {
+//     firstName: "babar",
+//   },
+//   {
+//     firstName: "Azam",
+//   },
+//   {
+//     firstName: "Shadab",
+//   },
+//   {
+//     firstName: "Hafeez",
+//   },
+// ];
+
+// const myFunc = (giveInput) => {
+//     for (let i = 0 ; i <= obj.length ; i++) {
+//         // console.log(obj[i].firstName);
+//         if( obj[i].firstName === "zzz" ) {
+//         console.log("sucess");
+//         }else{
+//             console.log("Try Again");
+//         }
+//     }
+// }
+
+// myFunc("zzz")
+
+// console.log(new Date());
+
+// console.log(typeof new Date());
+
