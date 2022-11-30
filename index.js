3db80120c22012d81499d31299e0e9e3114bf53b
adnanabrar786 <adnan1245abrar@gmail.com>
2022-11-30 15:27:11 -0800

    commit

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+//   ::::::::::::::::  Topic Name : typeof ::::::::::::::::
+
+typeof "John"; // Returns "string"
+typeof 3.14; // Returns "number"
+typeof NaN; // Returns "number"
+typeof false; // Returns "boolean"
+typeof [1, 2, 3, 4]; // Returns "object"
+typeof { name: "John", age: 34 }; // Returns "object"
+typeof new Date(); // Returns "object"
+typeof function () {}; // Returns "function"
+typeof myCar; // Returns "undefined" *
+typeof null; // Returns "object"
+
+
+//   ::::::::::::::::  Topic Name : Array.isArray([array]) ::::::::::::::::
+
+const myFruits = ["Banana", "Orange", "Apple", "Mango"];
+let resultArr =  Array.isArray(myFruits);
+// console.log(resultArr);
+
+
 //   ::::::::::::::::  Topic Name : Array.slice ::::::::::::::::
 
 const sliceArray = [1, 2, 3, 4, 5, 6, 7];
